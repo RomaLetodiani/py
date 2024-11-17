9c30463ddc231ccda8549c15d30b111f0467d9a5
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-17 20:29:16 +0400

    Refactor CSV to Excel conversion: update file paths and add JSON export functionality

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
 # Specify the path to your CSV file
-csv_file_path = 'files/Filtered_mega_investor_data.csv'
+csv_file_path = 'files/corrected_investors_data.csv'
 
-# Specify the desired path for the output Excel file
-xlsx_file_path = 'files/output_single_row.xlsx'
+file_path = 'files/output_single_row'
 
 # Read only the first row of the CSV file
 data = pd.read_csv(csv_file_path, nrows=1)
 
-# Write the single row to an Excel file
-data.to_excel(xlsx_file_path, index=False)
+def convert_to_json():
+    data.to_json(f'{file_path}.json', orient='records', lines=True)
+    print(f"Successfully converted to Excel and saved at: {file_path}.json")
 
-print(f"First row has been successfully converted to Excel and saved at: {xlsx_file_path}")
+def convert_to_excel():
+    data.to_excel(f'{file_path}.xlsx', index=False)
+    print(f"Successfully converted to Excel and saved at: {file_path}.xlsx")
