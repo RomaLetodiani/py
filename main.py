2e70ab9cd7247172a7bd10faec28cf4b337ad886
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-19 21:06:31 +0400

    Refactor data processing: switch from CSV to JSON input and implement investor analysis

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
 import pandas as pd
+import os
 
-# Specify the path to your CSV file
-csv_file_path = 'files/corrected_investors_data.csv'
+# Specify the path to your JSON file
+file_path = 'files/investor_110k_data.json'
 
-file_path = 'files/output_single_row'
+output_file_path = 'files/investor_110k_data'
 
-# Read only the first row of the CSV file
-data = pd.read_csv(csv_file_path, nrows=1)
+# Check if the file is not empty
+if os.path.getsize(file_path) > 0:
+    # Read the JSON file
+    data = pd.read_json(file_path)
+else:
+    raise ValueError("The JSON file is empty")
 
-def convert_to_json():
-    data.to_json(f'{file_path}.json', orient='records', lines=True)
-    print(f"Successfully converted to Excel and saved at: {file_path}.json")
+def analyze_data():
+    no_email_count = 0
+    yes_email_count = 0
+    no_linkedin_count = 0
+    yes_linkedin_count = 0
 
-def convert_to_excel():
-    data.to_excel(f'{file_path}.xlsx', index=False)
-    print(f"Successfully converted to Excel and saved at: {file_path}.xlsx")
+    yes_email_no_linkedin = 0
+    no_email_yes_linkedin = 0
+
+    yes_email_yes_linkedin = 0
+    no_email_no_linkedin = 0
+
+    # Iterate over the rows of the DataFrame
+    for _, investor in data.iterrows():
+        emails = investor['emails']
+        has_email = len(emails) > 0
+        has_linkedin = investor['linkedin'] is not None
+
+        if has_email:
+            yes_email_count += 1
+            if has_linkedin:
+                yes_email_yes_linkedin += 1
+            else:
+                yes_email_no_linkedin += 1
+        else:
+            no_email_count += 1
+            if has_linkedin:
+                no_email_yes_linkedin += 1
+            else:
+                no_email_no_linkedin += 1
+
+        if has_linkedin:
+            yes_linkedin_count += 1
+        else:
+            no_linkedin_count += 1
+
+    print(f"Total number of investors: {len(data)}")
+
+    print(f"Number of investors without email: {no_email_count}")
+    print(f"Number of investors without LinkedIn: {no_linkedin_count}")
+    print(f"Number of investors with email: {yes_email_count}")
+    print(f"Number of investors with LinkedIn: {yes_linkedin_count}")
+
+    print(f"Number of investors with email but no LinkedIn: {yes_email_no_linkedin}")
+    print(f"Number of investors with no email but with LinkedIn: {no_email_yes_linkedin}")
+    print(f"Number of investors with email and LinkedIn: {yes_email_yes_linkedin}")
+    print(f"Number of investors with no email and no LinkedIn: {no_email_no_linkedin}")
+
+# Run the analysis
+analyze_data()
